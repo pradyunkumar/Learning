01f8c1a2ac67e8a2410ecb35c3b9beb6ac733e02
pradyunkumar <43504266+pradyunkumar@users.noreply.github.com>
2020-01-05 18:11:28 -0800

    Add files via upload

---
+++ Numerics/Numerics.py
@@ -0,0 +1,26 @@
+# a = 15
+# b = 4
+# print(a/b)
+# print(a//b)
+#
+# for i in range(1,4):
+#     print(i)
+#
+# parrot = "Blue Red Green Sky"
+# print(parrot)
+# print(parrot[3])
+# print(parrot[-1])
+# print(parrot[1:2])
+# print(parrot[1:])
+# print(parrot[1:10:2])
+#
+# number = "9, 223, 372, 5, 92, 343"
+# print(number[1::4])
+print("he's" "probably " " pining")
+for i in range(1,5):
+    print("Hello")
+print("Hello1\n" * 5)
+today = "Friday"
+print("day" in today)
+print("Sunday" in today)
+
---
+++ StringFormatting/StringFormatting.py
@@ -0,0 +1,18 @@
+# age = 24
+# print("My age is " + str(age) + " years")
+#
+# print("My age is {0} years".format(age))
+#
+# print("""Mom- {0}
+# Dad- {1}
+# Pradyun- {1}
+# Tannavee- {0}
+# """.format("Girl", "Boy"))
+
+print("My english grade was %s, %d" % ("A-", 90))
+
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:<4} and cubed is {2:<4}".format(i,i**2,i**3))
+
+print("Pi is approcimately {0:12.50}".format(22/7))
+if __name__ == '__main__':
